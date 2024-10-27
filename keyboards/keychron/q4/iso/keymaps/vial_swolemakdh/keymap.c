f8103d02306c141ebe44f626921044820a477c53
EKlaman <43042371+EKlaman@users.noreply.github.com>
2024-10-27 23:39:07 +0100

    Add 6th layer, COLEMAKDH

--- keyboards/keychron/q4/iso/keymaps/vial_swolemakdh/keymap.c
+++ keyboards/keychron/q4/iso/keymaps/vial_swolemakdh/keymap.c
@@ -19,31 +19,39 @@
 
 // clang-format off
 bool user_led_enabled;
+bool colemak_ovveride;
+uint8_t default_layer; 
 HSV hsv;
 
 
 enum layers {
-    SWOLEMAKDH_BASE,
-    WIN_BASE,
+    WIN,
+    SWOLEMAKDH,
+    COLEMAKDH,
     _FN1,
     _FN2,
     _FN3
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [SWOLEMAKDH_BASE] = LAYOUT_iso_62(
-        KC_ESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       KC_7,      KC_8,    KC_9,    KC_0,     SE_PLUS,  SE_ACUT,  KC_BSPC,
-        KC_TAB,  KC_Q,     KC_W,    KC_F,    KC_P,    KC_B,    KC_J,       KC_L,      KC_U,    KC_Y,    SE_ODIA,  SE_ARNG,  SE_DIAE,
-        KC_CAPS, KC_A,     KC_R,    KC_S,    KC_T,    KC_G,    KC_M,       KC_N,      KC_E,    KC_I,    KC_O,     SE_ADIA,  SE_QUOT,  KC_ENT,
-        KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_D,    KC_V,    KC_NUBS,    KC_K,      KC_H,    SE_COMM, SE_DOT,   SE_MINS,            KC_RSFT,
-        KC_LCTL, KC_LGUI,  KC_LALT,                            KC_SPC,                                  KC_RALT,  MO(_FN1), MO(_FN2), TT(_FN3)),
-
-    [WIN_BASE] = LAYOUT_iso_62(
+    [WIN] = LAYOUT_iso_62(
         KC_ESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
         KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,
         KC_CAPS, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
         KC_LSFT, KC_NUBS,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_RSFT,
         KC_LCTL, KC_LGUI,  KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN1), MO(_FN2), TT(_FN3)),
+    [SWOLEMAKDH] = LAYOUT_iso_62(
+        KC_ESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       KC_7,      KC_8,    KC_9,    KC_0,     SE_PLUS,  SE_ACUT,  KC_BSPC,
+        KC_TAB,  KC_Q,     KC_W,    KC_F,    KC_P,    KC_B,    KC_J,       KC_L,      KC_U,    SE_ADIA, SE_ARNG,  SE_MINS,  SE_DIAE,
+        KC_CAPS, KC_A,     KC_R,    KC_S,    KC_T,    KC_G,    KC_M,       KC_N,      KC_E,    KC_I,    KC_O,     KC_Y,     SE_QUOT,  KC_ENT,
+        KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_D,    KC_V,    KC_NUBS,    KC_K,      KC_H,    SE_COMM, SE_DOT,   SE_ODIA,            KC_RSFT,
+        KC_LCTL, KC_LGUI,  KC_LALT,                            KC_SPC,                                  KC_RALT,  MO(_FN1), MO(_FN2), TT(_FN3)),
+    [COLEMAKDH] = LAYOUT_iso_62(
+        KC_ESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       KC_7,      KC_8,    KC_9,    KC_0,     SE_PLUS,  SE_ACUT,  KC_BSPC,
+        KC_TAB,  KC_Q,     KC_W,    KC_F,    KC_P,    KC_B,    KC_J,       KC_L,      KC_U,    KC_Y,    KC_LBRC,  KC_RBRC,  KC_NUBS,
+        KC_CAPS, KC_A,     KC_R,    KC_S,    KC_T,    KC_G,    KC_M,       KC_N,      KC_E,    KC_I,    KC_O,     KC_SCLN,  KC_QUOT,  KC_ENT,
+        KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_D,    KC_V,    KC_NUBS,    KC_K,      KC_H,    KC_COMM, KC_DOT,   KC_SLASH,           KC_RSFT,
+        KC_LCTL, KC_LGUI,  KC_LALT,                            KC_SPC,                                  KC_RALT,  MO(_FN1), MO(_FN2), TT(_FN3)),
 
     [_FN1] = LAYOUT_iso_62(
         SE_ACUT, KC_BRID, KC_BRIU, _______, _______,  _______, _______, _______, _______, _______, KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_DEL,
@@ -70,13 +78,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // [Post Init] --------------------------------------------------------------//
 void keyboard_post_init_user(void) {
+    default_layer = eeconfig_read_default_layer();
     // Set RGB to known state
     rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
-    rgb_matrix_sethsv_noeeprom(HSV_WHITE);
+    rgb_matrix_sethsv_noeeprom(212,200,rgb_matrix_config.hsv.v); 
+    rgb_matrix_enable_noeeprom();
     user_led_enabled = true;
 }
 
-void rgb_hsv_updata_user(void) {
+void rgb_hsv_update_user(void) {
     rgb_matrix_sethsv_noeeprom(rgb_matrix_config.hsv.h, rgb_matrix_config.hsv.s, rgb_matrix_config.hsv.v);
 }
 
@@ -94,7 +104,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
                 else {
                     rgb_matrix_config.hsv.v = RGB_MATRIX_MAXIMUM_BRIGHTNESS;
                 }
-                rgb_hsv_updata_user();
+                rgb_hsv_update_user();
             }
             return false;
         case RGB_VAD:
@@ -105,7 +115,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
                 else {
                     rgb_matrix_config.hsv.v = 0;
                 }
-                rgb_hsv_updata_user();
+                rgb_hsv_update_user();
             }
             return false;
         // Handle RGB Changes sans eeprom - necessary due to the layer dependent RGB color changes in matrix_scan_user
@@ -131,6 +141,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
 
 
 layer_state_t layer_state_set_user(layer_state_t state) {
+    default_layer = eeconfig_read_default_layer();
     switch (get_highest_layer(state)) {
         case _FN1:
             rgb_matrix_sethsv_noeeprom(0,255,rgb_matrix_config.hsv.v);
@@ -141,11 +152,11 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         case _FN3:
             rgb_matrix_sethsv_noeeprom(170,255,rgb_matrix_config.hsv.v);
             break;
-        default: // for any other layers, or the default layer
-            rgb_matrix_sethsv_noeeprom(0,0,rgb_matrix_config.hsv.v);
+        default:
+            rgb_matrix_sethsv_noeeprom(212,200,rgb_matrix_config.hsv.v);
             break;
     }
-  return state;
+    return state;  
 }
 // clang-format on
 
