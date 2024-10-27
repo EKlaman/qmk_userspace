4305f9bac3839575d6e8c514c782629a7b41fe89
EKlaman <43042371+EKlaman@users.noreply.github.com>
2024-10-27 19:39:55 +0100

    Remove ascii comment

--- keyboards/keychron/q4/iso/keymaps/vial_swolemakdh/keymap.c
+++ keyboards/keychron/q4/iso/keymaps/vial_swolemakdh/keymap.c
@@ -31,12 +31,6 @@ enum layers {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/*
-1 2 3 4 5 6 7 8 9 0 + ' | 1 2 3 4 5 6 7 8 9 0 + '
-q w f p b j l u y ö å ~ |  q w f p b j l u y ö å ~
-a r s t g m n e i o ä ' |   a r s t g m n e i o ä '
-z x c d v < k h , . -   |  z x c d v < k h , . -
-*/
     [SWOLEMAKDH_BASE] = LAYOUT_iso_62(
         KC_ESC,  KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       KC_7,      KC_8,    KC_9,    KC_0,     SE_PLUS,  SE_ACUT,  KC_BSPC,
         KC_TAB,  KC_Q,     KC_W,    KC_F,    KC_P,    KC_B,    KC_J,       KC_L,      KC_U,    KC_Y,    SE_ODIA,  SE_ARNG,  SE_DIAE,
